d6c3d67fced08ac7f856e5fcddcb95cd82d7afc0
Aum <aumkan23@gmail.com>
2023-10-28 21:21:25 +0530

    added dotenv

--- client/src/hooks/fetch.hooks.js
+++ client/src/hooks/fetch.hooks.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { getUsername } from "../helper/helper";
+import dotenv from 'dotenv';
 
+dotenv.config()
 axios.defaults.baseURL = process.env.REACT_APP_BASEURL;
 
 export default function useFetch(query) {
